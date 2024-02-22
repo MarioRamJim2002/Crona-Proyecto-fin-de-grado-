86e612529e0cbaf7039fb438914e14c101e080e6
Mario Ramirez Jimenez <marioramirez.lajarcia@gmail.com>
2024-02-22 07:12:33 +0100

    .

---
+++ Packages/Assets/Scripts/ArbustoManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ArbustoManager : MonoBehaviour
+{
+
+    public MainCharacter mainCharacter;
+    public Text textoRecoger;
+    public GameObject objetoInteraccion;
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
